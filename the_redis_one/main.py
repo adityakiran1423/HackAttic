1195fc7f756b62138b12dc37372b21247d12422e
adityakiran1423 <adi.kiran@gmail.com>
2025-09-03 19:13:09 +0530

    remove access token

--- the_redis_one/main.py
+++ the_redis_one/main.py
@@ -3,7 +3,7 @@ import pprint
 import requests
 
 def main():
-    url = "https://hackattic.com/challenges/the_redis_one/problem?access_token=a36030441954f10d"
+    url = "https://hackattic.com/challenges/the_redis_one/problem?access_token=xxx"
     response = requests.get(url)
     data = response.json()
     data = json.dumps(data, indent = 2)
